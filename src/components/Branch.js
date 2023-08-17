de72fbe1aa39f4e2a9c9b8ae7f3dd02b3f218cc3
Mikheil Choladze <mcholadze@hotmail.com>
2023-08-17 13:28:12 +0400

    Edit Branch fixed

--- src/components/Branch.js
+++ src/components/Branch.js
@@ -1,90 +1,102 @@
-import React from "react"
-import { useQuery, QueryClient, useQueries, useMutation, useQueryClient } from "@tanstack/react-query"
-import { AiFillEdit, AiFillDelete } from "react-icons/ai"
-import { Link, useNavigate } from 'react-router-dom'
-import * as Loader from "react-loader-spinner"
-import { EditBranch } from "../pages/Branch/EditBranch"
-import Swal from 'sweetalert2'
-import { toast } from 'react-toastify'
-import { removeBranch } from "../pages/Branch/branchApi"
+import React from "react";
+import {
+  useQuery,
+  QueryClient,
+  useQueries,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import * as Loader from "react-loader-spinner";
+import { EditBranch } from "../pages/Branch/EditBranch";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
+import { removeBranch } from "../pages/Branch/branchApi";
 
-const Branch = ({fullAddress, id, address, cityId}) => {
-    const navigate = useNavigate()
-    const queryClient = useQueryClient()
+const Branch = ({ fullAddress, id, address, cityId, city }) => {
+  // console.log("city", city);
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
-    const { mutateAsync } = useMutation(async (id) => {
-        await removeBranch(id)
-    },{
-        onSuccess: () => {queryClient.invalidateQueries(['pagedbranchlist'])
-            toast.success('ფილიალი წარმატებით წაიშალა!', {
-                theme: 'colored'
-            })},
-        onError: () => {
-            toast.error('ფილიალის წაშლისას მოხდა შეცდომა!', {
-                theme: 'colored'
-            })}
-    })
-    
-    let editButtonclicked = false
-
-    const editButtonHandlerClick = () => {
-        editButtonclicked = true
+  const { mutateAsync } = useMutation(
+    async (id) => {
+      await removeBranch(id);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["pagedbranchlist"]);
+        toast.success("ფილიალი წარმატებით წაიშალა!", {
+          theme: "colored",
+        });
+      },
+      onError: () => {
+        toast.error("ფილიალის წაშლისას მოხდა შეცდომა!", {
+          theme: "colored",
+        });
+      },
     }
+  );
 
-    const removeButtonHandlerClick = () => {
-        const swl = Swal.fire({
-            title: 'ნამდვილად გსურთ ფილიალის წაშლა?',
-            showConfirmButton: true,
-            showCancelButton: true,
-            confirmButtonColor: '#00cc66',
-            confirmButtonText: "დიახ",
-            cancelButtonText: "არა",
-            icon: 'warning',
-            // toast: true
-            //showLoaderOnConfirm: true,
-        }
-        )
-        .then((result) => {
-            if (result.isConfirmed ) {
-                mutateAsync(id)
-            } 
-            else if (result.isDismissed) {
-                toast.warning('თქვენ არ წაშალეთ ფილიალი', {
-                    theme: 'colored'
-                })
-            }
-            else {
-                toast.error('ფილიალის წაშლისას მოხდა შეცდომა', {
-                    theme: 'colored'
-                })
-            }
-        })
-        //.then(() => navigate('/branchlist/'))
-    }
+  let editButtonclicked = false;
 
-    return (
-        <div className='flex justify-between items-center p-2 my-2 border-[1px] border-red-500 rounded-lg'>
-        <div>{fullAddress}</div>
-        <div className='flex flex-col '>
-            <div >
-                <Link to={`/EditBranch/${id}`} state={{ fullAddress, id, address, cityId }} className='hover:text-lg hover:text-inherit'>
-                    <AiFillEdit className='text-2xl text-gray-500' >
-                        {/* <div className=''>
+  const editButtonHandlerClick = () => {
+    editButtonclicked = true;
+  };
+
+  const removeButtonHandlerClick = () => {
+    const swl = Swal.fire({
+      title: "ნამდვილად გსურთ ფილიალის წაშლა?",
+      showConfirmButton: true,
+      showCancelButton: true,
+      confirmButtonColor: "#00cc66",
+      confirmButtonText: "დიახ",
+      cancelButtonText: "არა",
+      icon: "warning",
+      // toast: true
+      //showLoaderOnConfirm: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        mutateAsync(id);
+      } else if (result.isDismissed) {
+        toast.warning("თქვენ არ წაშალეთ ფილიალი", {
+          theme: "colored",
+        });
+      } else {
+        toast.error("ფილიალის წაშლისას მოხდა შეცდომა", {
+          theme: "colored",
+        });
+      }
+    });
+    //.then(() => navigate('/branchlist/'))
+  };
+
+  return (
+    <div className="flex justify-between items-center p-2 my-2 border-[1px] border-red-500 rounded-lg">
+      <div>{fullAddress}</div>
+      <div className="flex flex-col ">
+        <div>
+          <Link
+            to={`/EditBranch/${id}`}
+            state={{ fullAddress, id, address, cityId, city }}
+            className="hover:text-lg hover:text-inherit"
+          >
+            <AiFillEdit className="text-2xl text-gray-500">
+              {/* <div className=''>
                             { editButtonclicked ? <Loader.ThreeDots color="red" height={10} /> : "რედაქტირება" }
                         </div> */}
-                    </AiFillEdit>
-                </Link>
-            </div>
-            
-            <div>
-                <button onClick={removeButtonHandlerClick}>
-                    <AiFillDelete className='text-2xl text-gray-500' />
-                </button>
-                
-            </div>
+            </AiFillEdit>
+          </Link>
         </div>
-    </div>
-    )
-}
 
-export { Branch }
+        <div>
+          <button onClick={removeButtonHandlerClick}>
+            <AiFillDelete className="text-2xl text-gray-500" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Branch };
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,62 +1,78 @@
-import React, {useState} from "react"
-import { Button, IconButton } from "@material-tailwind/react"
-import { ArrowRightIcon, ArrowLeftIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon, ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline"
-import { useQuery, QueryClient, useQueries, useMutation, useQueryClient } from "@tanstack/react-query"
-import { getPagedBranchList } from "../pages/Branch/branchApi"
-import { Branch } from './Branch'
+import React, { useState } from "react";
+import { Button, IconButton } from "@material-tailwind/react";
+import {
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/24/outline";
+import {
+  useQuery,
+  QueryClient,
+  useQueries,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { getPagedBranchList } from "../pages/Branch/branchApi";
+import { Branch } from "./Branch";
 
 export default function Pagination() {
   const [active, setActive] = useState(1);
 
   const { data, error, isLoading, isError, isPreviousData } = useQuery({
-    queryKey: ["pagedbranchlist", active], 
-    queryFn: () => getPagedBranchList(active), 
-    keepPreviousData: true
-   })
-   console.log("pagedbranchlist data: " ,data)
+    queryKey: ["pagedbranchlist", active],
+    queryFn: () => getPagedBranchList(active),
+    keepPreviousData: true,
+  });
+  // console.log("pagedbranchlist data: ", data);
 
-  const getItemProps = (index) =>
-    ({
-      variant: active === index ? "filled" : "text",
-      color: active === index ? "red" : "blue-gray",
-      onClick: () => {
-        if (active > (data?.count/data?.pageSize) + 1) return
-        setActive(index)
-      },
-    })
- 
+  const getItemProps = (index) => ({
+    variant: active === index ? "filled" : "text",
+    color: active === index ? "red" : "blue-gray",
+    onClick: () => {
+      if (active > data?.count / data?.pageSize + 1) return;
+      setActive(index);
+    },
+  });
+
   const next = () => {
-    if (active === data?.count/data?.pageSize) return
+    if (active === data?.count / data?.pageSize) return;
 
-    setActive(active + 1)
-  }
+    setActive(active + 1);
+  };
 
   const prev = () => {
     if (active === 1) return;
- 
-    setActive(active - 1)
-  }
+
+    setActive(active - 1);
+  };
 
   const end = () => {
-        
-    setActive(Math.ceil(data?.count/data?.pageSize))
-  }
+    setActive(Math.ceil(data?.count / data?.pageSize));
+  };
 
   const beginning = () => {
- 
-    setActive(1)
-  }
- 
+    setActive(1);
+  };
+
   return (
     <div>
       <div className="h-[380px]">
-      {data?.branches.map(({fullAddress, id, address, cityId }) => (
-        <Branch fullAddress={fullAddress} key={id} id={id} address={address} cityId={cityId}/>
-      ))}
+        {data?.branches.map(({ fullAddress, id, address, cityId, city }) => (
+          <Branch
+            fullAddress={fullAddress}
+            key={id}
+            id={id}
+            address={address}
+            cityId={cityId}
+            city={city}
+          />
+        ))}
       </div>
-        
-      <div className="flex items-center  mt-8">
 
+      <div className="flex items-center  mt-8">
         <Button
           variant="text"
           color="blue-gray"
@@ -64,7 +80,7 @@ export default function Pagination() {
           onClick={beginning}
           disabled={active === 1}
         >
-          <ChevronDoubleLeftIcon strokeWidth={2} className="h-4 w-4" /> 
+          <ChevronDoubleLeftIcon strokeWidth={2} className="h-4 w-4" />
           {/* Begining */}
         </Button>
 
@@ -75,41 +91,50 @@ export default function Pagination() {
           onClick={prev}
           disabled={active === 1}
         >
-          <ChevronLeftIcon strokeWidth={2} className="h-4 w-4" /> 
+          <ChevronLeftIcon strokeWidth={2} className="h-4 w-4" />
           {/* Previous */}
         </Button>
 
         <div className="flex items-center gap-1">
-
-          {
-            active <= 3 &&
+          {active <= 3 && (
             <>
               <IconButton {...getItemProps(1)}>1</IconButton>
               <IconButton {...getItemProps(2)}>2</IconButton>
-              <IconButton {...getItemProps(3)}>3</IconButton>
-              . . .
-              <IconButton {...getItemProps(Math.ceil(data?.count/data?.pageSize))}>{ Math.ceil(data?.count/data?.pageSize) }</IconButton>
+              <IconButton {...getItemProps(3)}>3</IconButton>. . .
+              <IconButton
+                {...getItemProps(Math.ceil(data?.count / data?.pageSize))}
+              >
+                {Math.ceil(data?.count / data?.pageSize)}
+              </IconButton>
             </>
-          }
-          {
-            active > 3 && active < (Math.ceil(data?.count/data?.pageSize)) &&
+          )}
+          {active > 3 && active < Math.ceil(data?.count / data?.pageSize) && (
             <>
-              <IconButton {...getItemProps(active - 2)}>{active - 2}</IconButton>
-              <IconButton {...getItemProps(active - 1)}>{active - 1}</IconButton>
-              <IconButton {...getItemProps(active)}>{active}</IconButton>
-              . . .
-              <IconButton {...getItemProps(Math.ceil(data?.count/data?.pageSize))}>{ Math.ceil(data?.count/data?.pageSize) }</IconButton>
+              <IconButton {...getItemProps(active - 2)}>
+                {active - 2}
+              </IconButton>
+              <IconButton {...getItemProps(active - 1)}>
+                {active - 1}
+              </IconButton>
+              <IconButton {...getItemProps(active)}>{active}</IconButton>. . .
+              <IconButton
+                {...getItemProps(Math.ceil(data?.count / data?.pageSize))}
+              >
+                {Math.ceil(data?.count / data?.pageSize)}
+              </IconButton>
             </>
-          }
-          {
-            active === (Math.ceil(data?.count/data?.pageSize)) &&
+          )}
+          {active === Math.ceil(data?.count / data?.pageSize) && (
             <>
-              <IconButton {...getItemProps(active - 2)}>{active - 2}</IconButton>
-              <IconButton {...getItemProps(active - 1)}>{active - 1}</IconButton>
+              <IconButton {...getItemProps(active - 2)}>
+                {active - 2}
+              </IconButton>
+              <IconButton {...getItemProps(active - 1)}>
+                {active - 1}
+              </IconButton>
               <IconButton {...getItemProps(active)}>{active}</IconButton>
             </>
-          }
-
+          )}
         </div>
 
         <Button
@@ -117,7 +142,7 @@ export default function Pagination() {
           color="blue-gray"
           className="flex items-center "
           onClick={next}
-          disabled={active >= data?.count/data?.pageSize}
+          disabled={active >= data?.count / data?.pageSize}
         >
           {/* Next */}
           <ChevronRightIcon strokeWidth={2} className="h-4 w-4" />
@@ -128,14 +153,12 @@ export default function Pagination() {
           color="blue-gray"
           className="flex items-center "
           onClick={end}
-          disabled={active >= data?.count/data?.pageSize}
+          disabled={active >= data?.count / data?.pageSize}
         >
           {/* End */}
           <ChevronDoubleRightIcon strokeWidth={2} className="h-4 w-4" />
         </Button>
-
       </div>
     </div>
-
   );
 }
